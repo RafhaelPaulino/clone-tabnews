a7e270db953617af309a82e89736828c2131762e
Rafhael Paulino <rafhaelshpaulino@gmail.com>
2025-11-16 01:10:32 +0000

    alteraçã de titulo da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home(){
-    return <h1>Eu te amo muitoo!, se você me ama da uma risadinha kkkk</h1>
+    return <h1>Eu te amo muitoo! Se você me ama da uma risadinha! 😂</h1>
 }
 
 export default home;
